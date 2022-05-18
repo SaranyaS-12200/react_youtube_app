e62010eb16dce058ec254719d49ef7ad27a414ce
SaranyaS-12200 <sharanya712pappu@gmail.com>
2022-05-18 11:26:17 +0530

    first commit

---
+++ client/src/components/views/UploadVideoPage/UploadVideoPage.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function UploadVideoPage() {
+  return (
+    <div>
+         Upload Video Page
+    </div>
+  )
+}
+
+export default UploadVideoPage
---
+++ server/config/dev.js
@@ -0,0 +1,3 @@
+module.exports = {
+    mongoURI:'mongodb+srv://mongodbsystem:<Welcome123>@cluster0.3ot6p.mongodb.net/?retryWrites=true&w=majority'
+}
